d3c404d0991cef224e5bfd18c0597704a8159700
enzo <enzo@enzo-B550M-DS3H>
2025-01-31 21:41:42 +0100

    C9 : Ajout de l'exercice 7

---
+++ src/exercice7/Account.java
@@ -0,0 +1,71 @@
+package exercice7;
+
+public class Account {
+	private String id;
+	private String name;
+	private int balance;
+	
+	public Account() {
+		id = "0";
+		name = "Michel Barnier";
+		balance = 0;
+	}
+	
+	public Account (String id, String name) {
+		this.id = id;
+		this.name = name;
+		balance = 0;
+	}
+	
+	public Account (String id, String name, int balance) {
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+
+
+	
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public int credit(int amount) {
+		balance += amount;
+		return balance ; 
+	}
+	
+	public int debit(int amount) {
+		if (amount <= balance) {
+			balance -= amount;
+		}
+		else
+			System.out.println("Amount exceeded balance");
+		return balance;
+	}
+
+	public int transferTo(Account another, int amount) {
+		if (amount <= balance) {
+			balance -= amount;
+			another.balance += amount;
+		}
+		else {
+			System.out.println("Amount exceeded balance");
+		}
+		return balance;
+		
+	}
+
+	public String toString() {
+		return "Account [id=" + id + ", name=" + name + ", balance=" + balance + "]";
+	}
+	
+}
